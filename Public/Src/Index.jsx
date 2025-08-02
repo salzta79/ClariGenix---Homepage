1d737dfc47a61e4774f20656210f7c7224dbd4b7
salzta79 <salztamannix@gmail.com>
2025-08-02 17:08:25 +1000

    Create Index.jsx

---
+++ Public/Src/Index.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import ClarigenixHome from "./ClarigenixHome";
+
+ReactDOM.render(<ClarigenixHome />, document.getElementById("root"));
